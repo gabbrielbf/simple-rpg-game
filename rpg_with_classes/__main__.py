d2dc4695d43dcb4914e695bdd6501da06c66c471
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-03 11:35:31 -0400

    test: archive main and battle system initiated

---
+++ rpg_with_classes/__main__.py
@@ -0,0 +1,12 @@
+from rpg_simples import *
+from rich import inspect
+
+def main():
+    """ Instânciando e iniciando o jogo """
+    jogo = Play()
+    jogo.inciar_batalha()
+    
+    return
+
+if __name__ == '__main__':
+    main()
--- rpg_with_classes/rpg_simples.py
+++ rpg_with_classes/rpg_simples.py
@@ -1,9 +1,12 @@
 class Personagem:
-    def __init__(self, nome, vida, nivel):
+    """ Classe responsável por 
+    instânciar e definir os personagens """
+    def __init__(self, nome, vida, nivel, mana):
         self.__nome = nome
         self.__vida = vida
         self.__nivel = nivel
-    
+        self.__mana = mana
+
     def get_nome(self):
         return self.__nome
     
@@ -13,29 +16,58 @@ class Personagem:
     def get_nivel(self):
         return self.__nivel
     
+    def get_mana(self):
+        return self.__mana
+    
     def exibir_dados(self):
-        return f'Nome: {self.get_nome()}\nVida: {self.get_vida()}\n{self.get_nivel()}'
+        return f'- Nome: {self.get_nome()}\n- Vida: {self.get_vida()}\n- Nivel: {self.get_nivel()}\n- Mana: {self.get_mana()}'
     
 
 class Protagonista(Personagem):
-    def __init__(self, nome, vida, nivel, habilidade):
-        super().__init__(nome, vida, nivel)
+    def __init__(self, nome, vida, nivel, mana, habilidade):
+        super().__init__(nome, vida, nivel, mana)
         self.__habilidade = habilidade
 
     def exibir_dados(self):
-        return f'{super().exibir_dados()}\nHabilidade: {self.get_habilidade()}\n'
+        return f'{super().exibir_dados()}\n- Habilidade: {self.get_habilidade()}\n'
 
     def get_habilidade(self):
         return self.__habilidade
 
 
 class Vilao(Personagem):
-    def __init__(self, nome, vida, nivel, tipo):
-        super().__init__(nome, vida, nivel)
+    def __init__(self, nome, vida, nivel, mana, tipo):
+        super().__init__(nome, vida, nivel, mana)
         self.__tipo = tipo
 
     def exibir_dados(self):
-        return f'{super().exibir_dados()}\Tipo: {self.get_tipo()}\n'
+        return f'{super().exibir_dados()}\n- Tipo: {self.get_tipo()}\n'
 
     def get_tipo(self):
         return self.__tipo
+    
+
+class Play:
+    """ Classe responsável por 
+    inciar e orquestrar o jogo """
+    def __init__(self):
+        self.protagonista = Protagonista(nome='Goku', vida=100, nivel=5, mana=60 ,habilidade='Kamehameha')
+        self.vilao = Vilao(nome='Coringa', vida=160, nivel=3, mana=30, tipo='Assassino')
+
+    def inciar_batalha(self):
+        """ Gestiona as batalhas 
+        em turno estilo Pokémon """
+        print('='*19)
+        print('|Iniciando batalha|')
+        print('='*19)
+
+        while self.protagonista.get_vida() > 0 and self.vilao.get_vida() > 0:
+            print('Personagens:')
+            print(self.protagonista.exibir_dados())
+            print(self.vilao.exibir_dados())
+
+            input('Press ENTER...')
+            print('1 - Ataque normal')
+            print('2 - Ataque especial')
+            opcao = input('Digite aqui -> ')
+            
