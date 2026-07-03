1aad30942e5926973d4ae0d7e37041e02ea7c76a
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-03 00:19:42 -0300

    feat: implemented getters, setters and exibition of data bases

--- rpg_with_classes/rpg_simples.py
+++ rpg_with_classes/rpg_simples.py
@@ -13,3 +13,29 @@ class Personagem:
     def get_nivel(self):
         return self.__nivel
     
+    def exibir_dados(self):
+        return f'Nome: {self.get_nome()}\nVida: {self.get_vida()}\n{self.get_nivel()}'
+    
+
+class Protagonista(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade):
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+    def exibir_dados(self):
+        return f'{super().exibir_dados()}\nHabilidade: {self.get_habilidade()}\n'
+
+    def get_habilidade(self):
+        return self.__habilidade
+
+
+class Vilao(Personagem):
+    def __init__(self, nome, vida, nivel, tipo):
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def exibir_dados(self):
+        return f'{super().exibir_dados()}\Tipo: {self.get_tipo()}\n'
+
+    def get_tipo(self):
+        return self.__tipo
